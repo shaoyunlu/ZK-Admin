f2d0fe80369a55746ed1984157d8fcfe26d45c88
shaoyunlu <shaoyunlu@126.com>
2024-03-06 23:47:22 +0800

    1

--- init.sql
+++ init.sql
@@ -1,8 +1,18 @@
-CREATE TABLE user (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL,
-    UNIQUE (username),
-    UNIQUE (email)
+DROP TABLE IF EXISTS User;
+
+CREATE TABLE User (
+    user_id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    user_name VARCHAR(255) NOT NULL,
+    user_password VARCHAR(255) NOT NULL,
+    user_email VARCHAR(255) NOT NULL,
+    user_update_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    UNIQUE (user_name),
+    UNIQUE (user_email)
+);
+
+DROP TABLE IF EXISTS Role;
+CREATE TABLE Role (
+    role_id INT PRIMARY KEY AUTO_INCREMENT,
+    role_name VARCHAR(255) NOT NULL,
+    role_desc TEXT
 );
