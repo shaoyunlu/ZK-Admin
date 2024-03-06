77a90d6fdb1179ed4459d87e20993d5543af05bf
shaoyunlu <shaoyunlu@126.com>
2024-03-07 00:01:36 +0800

    1

--- init.sql
+++ init.sql
@@ -1,7 +1,11 @@
-DROP TABLE IF EXISTS User;
+DROP TABLE IF EXISTS R_UserRole;
+DROP TABLE IF EXISTS R_RolePermission;
+DROP TABLE IF EXISTS C_User;
+DROP TABLE IF EXISTS C_Role;
+DROP TABLE IF EXISTS C_Permission;
 
-CREATE TABLE User (
-    user_id BIGINT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE C_User (
+    user_id INT AUTO_INCREMENT PRIMARY KEY,
     user_name VARCHAR(255) NOT NULL,
     user_password VARCHAR(255) NOT NULL,
     user_email VARCHAR(255) NOT NULL,
@@ -10,9 +14,32 @@ CREATE TABLE User (
     UNIQUE (user_email)
 );
 
-DROP TABLE IF EXISTS Role;
-CREATE TABLE Role (
+
+CREATE TABLE C_Role (
     role_id INT PRIMARY KEY AUTO_INCREMENT,
     role_name VARCHAR(255) NOT NULL,
     role_desc TEXT
+);
+
+
+CREATE TABLE C_Permission (
+    permission_id INT PRIMARY KEY AUTO_INCREMENT,
+    permission_name VARCHAR(255) NOT NULL,
+    Description TEXT
+);
+
+CREATE TABLE R_UserRole (
+    ur_user_id INT,
+    ur_role_id INT,
+    PRIMARY KEY (ur_user_id, ur_role_id),
+    FOREIGN KEY (ur_user_id) REFERENCES C_User(user_id),
+    FOREIGN KEY (ur_role_id) REFERENCES C_Role(role_id)
+);
+
+CREATE TABLE R_RolePermission (
+    rp_role_id INT,
+    rp_permission_id INT,
+    PRIMARY KEY (rp_role_id, rp_permission_id),
+    FOREIGN KEY (rp_role_id) REFERENCES C_Role(role_id),
+    FOREIGN KEY (rp_permission_id) REFERENCES C_Permission(permission_id)
 );
